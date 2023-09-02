226b7f3ad61bd5144ca7d4d8555aff5a79094c13
Mo9932 <mbakr9932@gmail.com>
2023-09-02 23:50:12 +0300

    The last version

--- AVR_GAME_PROJECT/Special.c
+++ AVR_GAME_PROJECT/Special.c
@@ -13,18 +13,27 @@
 #include "LCD.h"
 #include "Special.h"
 
-u8 Dino_0[7][8]= Dino;
+u8 Dino_0[7][8]= Dino;//First Frame
 
-u8 Dino_1[7][8]= Dino_Move;
+u8 Dino_1[7][8]= Dino_Move;//Second Frame
 
-void LCD_PrintDino(u8 *Copy_State)
+u8 Tree_0[8] = Tree;//Tree Shape
+
+
+/*This Function Print the frames corresponding to the game state , delay and update the location of the tree
+ * Calling by reference is used to access the global variables in main function */
+void LCD_PrintDino(u8 *Copy_State,u8 *Location,u8 Animation_Delay)
 {
 	u8 count;
+	static u8 Flag=0;
+	static u8 Score=0;
+	LCD_VidSpecialChar(7,Tree_0);//Put the tree shape in CGRAM in this location
 	if(*Copy_State==0)
 	{
+		Flag=0;
 		for(count=0;count<7;count++)
 		{
-			LCD_VidSpecialChar(count,Dino_0[count]);
+			LCD_VidSpecialChar(count,Dino_0[count]);//Put the Dino_0 shape in CGRAM in this location
 		}
 		for(count=0;count<7;count++)
 		{
@@ -39,10 +48,18 @@ void LCD_PrintDino(u8 *Copy_State)
 				LCD_VidSendChar(count);
 			}
 		}
-		_delay_ms(200);
+		LCD_VidSendLocation(LINE_4,19-((*Location)-1));//Go to the location before tree
+		LCD_VidSendChar(0xfe);						   //Print NULL Character
+		LCD_VidSendLocation(LINE_4,19-(*Location));	   //Go to the tree location
+		LCD_VidSendChar(7);							   //Print the tree
+		(*Location)++;								   //Update the tree location
+		_delay_ms(Animation_Delay);
+		/*if the tree is at the end of the screen reset the location to 0*/
+		if((*Location)==20)
+			(*Location)=0;
 		for(count=0;count<7;count++)
 		{
-			LCD_VidSpecialChar(count,Dino_1[count]);
+			LCD_VidSpecialChar(count,Dino_1[count]);//Put the Dino_1 shape in CGRAM in this location
 		}
 		for(count=0;count<7;count++)
 		{
@@ -57,11 +74,36 @@ void LCD_PrintDino(u8 *Copy_State)
 				LCD_VidSendChar(count);
 			}
 		}
-		_delay_ms(200);
+		LCD_VidSendLocation(LINE_4,19-((*Location)-1));
+		LCD_VidSendChar(0xfe);
+		LCD_VidSendLocation(LINE_4,19-(*Location));
+		LCD_VidSendChar(7);
+		(*Location)++;
+		_delay_ms(Animation_Delay);
+		if((*Location)==20)
+			(*Location)=0;
 	}
+	/*If game state is 1 make the dino jump
+	 * Print the first frame 2-Lines higher
+	 * Wait 4 frames and reset the state to 0*/
 	if(*Copy_State==1)
 	{
-		LCD_VidClear();
+		if(Flag==0)
+		{
+			for(count=0;count<7;count++)
+			{
+				if(count<4)
+				{
+					LCD_VidSendLocation(LINE_3,count+1);
+					LCD_VidSendChar(0xfe);
+				}
+				if(count>3)
+				{
+					LCD_VidSendLocation(LINE_4,count-3);
+					LCD_VidSendChar(0xfe);
+				}
+			}
+		}
 		for(count=0;count<7;count++)
 		{
 			LCD_VidSpecialChar(count,Dino_0[count]);
@@ -70,18 +112,75 @@ void LCD_PrintDino(u8 *Copy_State)
 		{
 			if(count<4)
 			{
-				LCD_VidSendLocation(LINE_1,count+1);
+				LCD_VidSendLocation(LINE_2,count+1);
 				LCD_VidSendChar(count);
 			}
 			if(count>3)
 			{
-				LCD_VidSendLocation(LINE_2,count-3);
+				LCD_VidSendLocation(LINE_3,count-3);
 				LCD_VidSendChar(count);
 			}
 		}
-		_delay_ms(200);
-		LCD_VidClear();
-		*Copy_State=0;
+		/*the same as in state 0*/
+		LCD_VidSendLocation(LINE_4,19-((*Location)-1));
+		LCD_VidSendChar(0xfe);
+		LCD_VidSendLocation(LINE_4,19-(*Location));
+		LCD_VidSendChar(7);
+		(*Location)++;
+		_delay_ms(Animation_Delay);
+
+
+		Flag++;
+		/*if the flag is 4 reset the flag and state*/
+		if(Flag==4)
+		{
+			*Copy_State=0;
+			Flag=0;
+			for(count=0;count<7;count++)
+			{
+				if(count<4)
+				{
+					LCD_VidSendLocation(LINE_2,count+1);
+					LCD_VidSendChar(0xfe);
+				}
+				if(count>3)
+				{
+					LCD_VidSendLocation(LINE_3,count-3);
+					LCD_VidSendChar(0xfe);
+				}
+			}
+		}
+		if((*Location)==20)
+			(*Location)=0;
+
+		if((*Location)==18)
+			Score++;
 	}
+
+	/*Game over state*/
+	if(*Copy_State==2)
+	{
+		/*Clear the screen just one time*/
+		if(Flag!=3)
+		{
+			LCD_VidClear();
+		}
+
+		LCD_VidSendLocation(LINE_2,4);
+		LCD_VidSendString("Game Over");
+		LCD_VidSendLocation(LINE_3,4);
+		LCD_VidSendString("Your Score:");
+		/*Print the score one time before resetting */
+		if(Flag!=3)
+			LCD_VidSendNumber(Score);
+
+		LCD_VidSendLocation(LINE_4,2);
+		LCD_VidSendString("Press * to Play");
+		Flag=3; //Update the flag to print the score and clear the screen for one time
+		Score=0;//Reset the score
+	}
+
+	LCD_VidSendLocation(LINE_4,0);
+	LCD_VidSendChar(0xfe);
 
 }
--- AVR_GAME_PROJECT/main.c
+++ AVR_GAME_PROJECT/main.c
@@ -17,6 +17,7 @@
 #include "DIO_INT.h"
 
 u8 State=0;
+u8 Location=0;
 
 void Set_State(void);
 int main(void)
@@ -26,16 +27,44 @@ int main(void)
 	DIO_VidSetPinValue(PORT_D,PIN_2,DIO_HIGH);
 	LCD_VidInt();
 	KPD_VidInt();
-	INT_Int();
-	EXT_INTADD(INT_0,Set_State);
-    INT_GIE();
+	INT_Int();										//Enable the interrupt
+	EXT_INTADD(INT_0,Set_State);					//If the interrupt flag is high call the state function
+    INT_GIE();										//Enable the global interrupt flag
 
 	while(1)
 	{
-		LCD_PrintDino(&State);
+		/*To check if the Dino is above the tree and not jumping*/
+		if((Location==18 || Location==17 || Location==16 || Location==15)&& State==0)
+		{
+			State=2;
+			/*Disable the interrupt
+			 * If the user tried to make the dino jump the state will not be updated*/
+			CLEAR_BIT(SREG,7);
+		}
+		else
+		{
+			LCD_PrintDino(&State,&Location,80);//Take and update game state ,Update tree location and take the animation delay
+		}
+
+		if(State==2)
+		{
+			Key=KPD_u8PressedKey();
+			while(Key!=KPD_CHEK)
+			{
+				if(Key=='c')
+				{
+					State=0;
+					Location=0;
+					LCD_VidClear();
+					INT_GIE();
+					Key=KPD_CHEK;
+				}
+			}
+		}
 	}
 }
 
+/*Update the state to make the dino jump*/
 void Set_State(void)
 {
 	State=1;
